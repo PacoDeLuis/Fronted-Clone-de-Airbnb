48205a493303dbf2c8d570eb90254aee1104e959
Paco De Luis <ponchoflores464@gmail.com>
2025-03-04 09:52:02 -0600

    12 create reusable modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
-
+import Modal from "./components/modals/Modal";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const content = (
-    <p>Yo yo</p>
+    <p>Yo</p>
   )
 
   return (
@@ -29,7 +29,7 @@ export default function RootLayout({
         <div className="pt-32">
           {children}
         </div>
-
+        
       </body>
     </html>
   );
