cd8c2f6037744435de7f236ca21e87094cf67e44
Paco De Luis <ponchoflores464@gmail.com>
2025-03-13 09:36:20 -0600

    19 log out

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 import MenuLink from "./MenuLink";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useSignupModal from "@/app/hooks/useSignupModal";
+import LogoutButton from "../LogoutButton";
 
 
-const UserNav = () => {
+interface UserNavProps {
+    userId?: string | null
+}
+
+const UserNav:React.FC<UserNavProps> = ({
+    userId
+}) => {
     const loginModal = useLoginModal()
     const signupModal = useSignupModal()
 
@@ -26,20 +33,27 @@ const UserNav = () => {
             </button>
             {isOpen && (
                 <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
-                    <MenuLink
-                        label='Log in'
-                        onClick={() => {
-                         setIsOpen(false)
-                         loginModal.open()
+                    {userId ?(
+                        <LogoutButton/>
+                    ): 
+                    <>
+                        <MenuLink
+                            label='Log in'
+                            onClick={() => {
+                                setIsOpen(false)
+                                loginModal.open()
                         }}
                     />
-                    <MenuLink
-                        label='Sign up'
-                        onClick={() => {
-                         setIsOpen(false)
-                         signupModal.open()
+                        <MenuLink
+                            label='Sign up'
+                            onClick={() => {
+                                setIsOpen(false)
+                                signupModal.open()
                         }}
                     />
+                </>
+        }
+                    
                 </div>
             )}
         </div>
